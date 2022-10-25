53ce0be1379c6bb231129188526843e186b99e3b
monkeyMax-SVG <monkeymax@yeah.net>
2022-10-25 18:32:44 +0800

    deployed nft reward system on bsc testnet for test purpose

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,22 +1,26 @@
 async function main() {
   const [deployer] = await ethers.getSigners();
+  console.log("Deployer: ", deployer.address);
   
   // Deploy TAVA Token contract
   const TAVA = await ethers.getContractFactory("TAVA");
   const TAVAContract = await TAVA.deploy();
   await TAVAContract.deployed();
+  console.log("TAVAContract: ", TAVAContract.address);
 
   // Deploy SecondskinNFT contract
   const SecondSkinNFT = await ethers.getContractFactory("SecondSkinNFT");
   const SecondSkinNFTContract = await SecondSkinNFT.deploy(
-    owner.address
+    deployer.address
   );
   await SecondSkinNFTContract.deployed();
+  console.log("SecondSkinNFT: ", SecondSkinNFTContract.address);
 
   // Deploy ThirdParty NFT Generator contract
   const NFTFactory = await ethers.getContractFactory("NFTFactory");
   const NFTFactoryContract = await NFTFactory.deploy();
   await NFTFactoryContract.deployed();
+  console.log("NFTFactory: ", NFTFactoryContract.address);
 
   // Deploy NFTMasterChef contract
   const NFTMasterChef = await ethers.getContractFactory("NFTMasterChef");
@@ -25,6 +29,7 @@ async function main() {
     SecondSkinNFTContract.address
   );
   await NFTMasterChefContract.deployed();
+  console.log("NFTMasterChef: ", NFTMasterChefContract.address);
 }
 
 main()
--- verify.sh
+++ verify.sh
@@ -1,2 +1,11 @@
+# TAVA Token
+npx hardhat verify --contract contracts/TAVA.sol:TAVA --network testnet 0xae5817a9Ebe5210Fe97FBFe2741e0E284a76bD6a
+
+# SecondSkinNFT
+npx hardhat verify --network testnet 0x53252d4910174571c97FC0F1E314065B17020dfF "0x74BF843856effC69aeB637f5d8d28da86D6f0a53"
+
+# NFTFactory
+npx hardhat verify --network testnet 0x0137492b8dB705c68E54855EfD04D85D4d2BC8CA
+
 # NFTMasterChef ()
-# npx hardhat verify --network testnet 0xBf6232b66dcCfA5EFCd43F0bcAEd743e21822
+npx hardhat verify --network testnet 0x36D4Be2a04bf3c7c4ea23797288066339680B5f7 "0xae5817a9Ebe5210Fe97FBFe2741e0E284a76bD6a" "0x53252d4910174571c97FC0F1E314065B17020dfF"
